eb9743c1da523aaf505499dee3f0769af77d238c
Nicola Musatti <nicola.musatti@gmail.com>
2019-01-20 17:00:35 +0100

    Added test

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -12,3 +12,13 @@ def test_hosts_file(host):
     assert f.exists
     assert f.user == 'root'
     assert f.group == 'root'
+
+
+def test_cobc(host):
+    f = host.file('/opt/gnucobol-3.0/bin/cobc')
+    assert f.exists
+    assert f.user == 'gnucobol'
+    assert f.group == 'gnucobol'
+
+    host.check_output('/opt/gnucobol-3.0/bin/cobc --version').find(
+        '3.0-rc1.0') > -1
